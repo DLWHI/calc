a6657df537a8e1cf62ea3901f76fb374224fa87a
dlwhi <samimmirtal999@gmail.com>
2023-07-14 20:00:35 +0300

    deleted build/fixed main

--- main.cpp
+++ main.cpp
@@ -8,48 +8,48 @@
 int main(int argc, char *argv[])
 {
     s21::Translator tr;
-    // std::cout << tr.fix("sin()cos()") << std::endl;
-    // std::cout << tr.fix("2x") << std::endl;
-    // std::cout << tr.fix("x2") << std::endl;
-    // std::cout << tr.fix(")(") << std::endl;
-    // std::cout << tr.fix("2(") << std::endl;
-    // std::cout << tr.fix("sin()(") << std::endl;
-    // std::cout << tr.fix(")cos()") << std::endl;
-    // std::cout << tr.fix("2sin") << std::endl;
-    // std::cout << tr.fix("xsin") << std::endl;
-    // std::cout << tr.fix("sin()x") << std::endl;
-    // std::cout << tr.fix("sin()2") << std::endl;
-    // std::cout << tr.fix("xx") << std::endl;
-    // std::cout << tr.fix("22") << std::endl;
+    std::cout << tr.Fix("sin()cos()") << std::endl;
+    std::cout << tr.Fix("2x") << std::endl;
+    std::cout << tr.Fix("x2") << std::endl;
+    std::cout << tr.Fix(")(") << std::endl;
+    std::cout << tr.Fix("2(") << std::endl;
+    std::cout << tr.Fix("sin()(") << std::endl;
+    std::cout << tr.Fix(")cos()") << std::endl;
+    std::cout << tr.Fix("2sin") << std::endl;
+    std::cout << tr.Fix("xsin") << std::endl;
+    std::cout << tr.Fix("sin()x") << std::endl;
+    std::cout << tr.Fix("sin()2") << std::endl;
+    std::cout << tr.Fix("xx") << std::endl;
+    std::cout << tr.Fix("22") << std::endl;
     // std::cout << std::endl;
-    // std::cout << tr.fix("sin(2") << std::endl;
-    // std::cout << tr.fix("sin2x") << std::endl;
-    // std::cout << tr.fix("sinxcos2") << std::endl;
-    // std::cout << tr.fix("sin)cos(0") << std::endl;
-    // std::cout << tr.fix("sin(2") << std::endl;
-    // std::cout << tr.fix("sincos8") << std::endl;
-    // std::cout << tr.fix(")cos()") << std::endl;
+    // std::cout << tr.Fix("sin(2") << std::endl;
+    // std::cout << tr.Fix("sin2x") << std::endl;
+    // std::cout << tr.Fix("sinxcos2") << std::endl;
+    // std::cout << tr.Fix("sin)cos(0") << std::endl;
+    // std::cout << tr.Fix("sin(2") << std::endl;
+    // std::cout << tr.Fix("sincos8") << std::endl;
+    // std::cout << tr.Fix(")cos()") << std::endl;
 
-    std::cout << tr.fix("2sin") << std::endl; // add *
-    std::cout << tr.fix("2(") << std::endl; // add *
-    std::cout << tr.fix("2x") << std::endl; // add *
+    std::cout << tr.Fix("2sin") << std::endl; // add *
+    std::cout << tr.Fix("2(") << std::endl; // add *
+    std::cout << tr.Fix("2x") << std::endl; // add *
     
-    std::cout << tr.fix("++") << std::endl; // check for unary
-    std::cout << tr.fix("+)") << std::endl; // throw exception maybe
+    std::cout << tr.Fix("++") << std::endl; // check for unary
+    std::cout << tr.Fix("+)") << std::endl; // throw exception maybe
     
-    std::cout << tr.fix("sin2") << std::endl; // add brackets 
-    std::cout << tr.fix("sin+") << std::endl; // add brackets
-    std::cout << tr.fix("sincos") << std::endl; // add brackets
-    std::cout << tr.fix("sin)") << std::endl; // fix brackets
-    std::cout << tr.fix("sinx") << std::endl; // add brackets
+    std::cout << tr.Fix("sin2") << std::endl; // add brackets 
+    std::cout << tr.Fix("sin+") << std::endl; // add brackets
+    std::cout << tr.Fix("sincos") << std::endl; // add brackets
+    std::cout << tr.Fix("sin)") << std::endl; // Fix brackets
+    std::cout << tr.Fix("sinx") << std::endl; // add brackets
 
-    std::cout << tr.fix("(+") << std::endl; // make unary
-    std::cout << tr.fix("()") << std::endl; // x throw exception or fix
+    std::cout << tr.Fix("(+") << std::endl; // make unary
+    std::cout << tr.Fix("()") << std::endl; // x throw exception or Fix
 
-    std::cout << tr.fix(")2") << std::endl; // add *
-    std::cout << tr.fix(")sin") << std::endl; // add *
-    std::cout << tr.fix(")(") << std::endl; // add *
-    std::cout << tr.fix(")x") << std::endl; // add *
+    std::cout << tr.Fix(")2") << std::endl; // add *
+    std::cout << tr.Fix(")sin") << std::endl; // add *
+    std::cout << tr.Fix(")(") << std::endl; // add *
+    std::cout << tr.Fix(")x") << std::endl; // add *
     return 0;
 }
 
