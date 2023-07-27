827262f3824fbacb9983da8536727fa376fb0622
Lacresha Emil <lacresha@il-a3.msk.21-school.ru>
2023-07-27 16:48:20 +0300

    added controller to main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include "src/model/DefaultModel.h"
 #include "src/view/mainwindow.h"
+#include "src/controller/CalcModelController.h"
 
 #include <QApplication>
 #include <iostream>
@@ -7,6 +9,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    s21::DefaultModel model;
+    s21::CalcModelController troller(&model, &w);
     w.show();
     return a.exec();
 }
